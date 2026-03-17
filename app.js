15118356da2146386311d7092e4c52e4b0929269
garand3r <martinbilluy@gmail.com>
2026-03-17 18:24:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ document.getElementById("loginCard").classList.remove("hidden");
 function registerVisitor(){
 
 const name=document.getElementById("name").value.trim();
-const zone=document.getElementById("zone").value;
+const zone1=document.getElementById("zone1").value;
+const zone2=document.getElementById("zone2").value;
+const zone3=document.getElementById("zone3").value;
 
 if(!name){
 alert("Please enter visitor name");
@@ -49,7 +51,9 @@ return;
 const visitor={
 id:Date.now(),
 name,
-zone,
+zone1,
+zone2,
+zone3,
 startTime:Date.now(),
 timedOut:false
 };
@@ -88,11 +92,18 @@ if(v.timedOut)div.classList.add("timeout");
 
 const timeLeft=getTimeLeft(v.startTime);
 
+let zones=v.zone1;
+
+if(v.zone2!=="None") zones+=" , "+v.zone2;
+if(v.zone3!=="None") zones+=" , "+v.zone3;
+
 div.innerHTML=`
 <strong>${v.name}</strong><br>
-Zone: ${v.zone}<br>
+Zones: ${zones}<br>
 ${v.timedOut ? "⚠ Timed Out" : "Time Left: "+timeLeft}
-<br><button onclick="printReceiptById(${v.id})">Reprint</button>
+<br>
+<button onclick="printReceiptById(${v.id})">Reprint</button>
+${v.timedOut ? `<button class="delete-btn" onclick="deleteVisitor(${v.id})">Delete</button>` : ""}
 `;
 
 container.appendChild(div);
@@ -100,6 +111,16 @@ container.appendChild(div);
 });
 
 saveData();
+
+}
+
+function deleteVisitor(id){
+
+visitors=visitors.filter(v=>v.id!==id);
+
+saveData();
+
+loadVisitors();
 
 }
 
@@ -116,6 +137,11 @@ return `${m}:${s.toString().padStart(2,"0")}`;
 
 function printReceipt(visitor){
 
+let zones=visitor.zone1;
+
+if(visitor.zone2!=="None") zones+="\n"+visitor.zone2;
+if(visitor.zone3!=="None") zones+="\n"+visitor.zone3;
+
 const receipt=document.getElementById("receiptContent");
 
 receipt.innerHTML=`
@@ -123,22 +149,22 @@ receipt.innerHTML=`
 <b>CAFEQUEUE</b><br>
 Art Museum Cafe
 
-<div id="line"></div>
+<div class="line"></div>
 
 Visitor:<br>
 ${visitor.name}<br><br>
 
-Zone:<br>
-${visitor.zone}<br><br>
+Zones:<br>
+${zones.replace(/\n/g,"<br>")}<br>
 
 Time Limit: 15 Minutes
 
-<div id="line"></div>
+<div class="line"></div>
 
 Issued:<br>
 ${new Date().toLocaleString()}
 
-<div id="line"></div>
+<div class="line"></div>
 
 Enjoy Your Visit!
 
@@ -162,10 +188,10 @@ printReceipt(visitor);
 
 function exportCSV(){
 
-let csv="Name,Zone,Timed Out\n";
+let csv="Name,Zone1,Zone2,Zone3,Timed Out\n";
 
 visitors.forEach(v=>{
-csv+=`"${v.name}","${v.zone}","${v.timedOut}"\n`;
+csv+=`"${v.name}","${v.zone1}","${v.zone2}","${v.zone3}","${v.timedOut}"\n`;
 });
 
 const blob=new Blob([csv],{type:"text/csv"});
